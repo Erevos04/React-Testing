8345962778a20723ca0f8ab31887d84b0d1b204b
Orestis <dim.orest04@gmail.com>
2018-07-09 01:24:28 +0300

    v 0.2

--- src/App.js
+++ src/App.js
@@ -8,11 +8,10 @@ class App extends Component {
   constructor(props){
     super(props)
     this.state = {
-      users : [],
-      gridData: this.users,
+      gridData: Object,
       loading: false
-    }
   }
+}
 
 getUsers(){
   this.setState({
@@ -20,47 +19,45 @@ getUsers(){
   })
   axios('https://api.randomuser.me/?nat=US&results=5')
   .then(response => this.setState({
-    users: [...this.state.users, ...response.data.results],
-    gridData: [...this.state.users, ...response.data.results],
+    gridData: {
+      users: [...this.state.gridData, ...response.data.results]
+    },
     loading: false,
   }));
-
 }
 
-
-  componentWillMount(){
-      this.getUsers();
-  }
+componentWillMount(){
+  this.getUsers();
+}
 
 handleSubmit = (e) => {
   e.preventDefault();
   this.getUsers();
 }
 
-  render() {
-    const {loading, users, gridData} = this.state
-    return (
-      <div className="App">
-        <form onSubmit={this.handleSubmit}>
-          <input type="submit" value="Load More Users"/>
-        </form>
-        <hr />
-        {!loading ? (
-          <div>
-            <Grid
-              style = {{ height: '400px' }}
-              data = {gridData}
-          >
-              <Column field="name.first" title="First Name" width="150px" />
-              <Column field="name.last" title="Last Name" width="150px" />
-              <Column field="location.city" title="City" width="200px" />
-              <Column field="cell" title="Cellphone" width="200px" />
-              <Column field="email" title="Email" width="300" />
-              <Column field="registered.date" title="Reg Date" width="300px" />
-            </Grid>
-          </div>
+render() {
+  const {loading, gridData} = this.state
+  return (
+    <div className="App">
+      <form onSubmit={this.handleSubmit}>
+        <input type="submit" value="Load More Users"/>
+      </form>
+      <hr />
+      {!loading ? (
+        <div>
+          <Grid
+            style = {{ height: '400px' }}
+            data = {gridData.users}>
+            <Column field="name.first" title="First Name" width="150px" />
+            <Column field="name.last" title="Last Name" width="150px" />
+            <Column field="location.city" title="City" width="200px" />
+            <Column field="cell" title="Cellphone" width="200px" />
+            <Column field="email" title="Email" width="300" />
+            <Column field="registered.date" title="Reg Date" width="300px" />
+          </Grid>
+        </div>
         ) : <Loading message="Loading users!"/> }
-      </div>
+        </div>
     );
   }
 }
